aca5f76314ef4a3c5d45f3a267dcd850d6696de9
Rana <rantun@udel.edu>
2024-04-02 11:21:26 -0400

    Created main menu and level select screens

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -18,6 +18,41 @@ export default class MainScene extends Phaser.Scene {
                 fontSize: "24px",
             })
             .setOrigin(1, 0);
+
+        this.add.text(180, 200, "Boolean Bonanza (add img)", {
+            fontSize: "32px",
+            color: "black",
+        });
+
+        //Possible reference for some features: https://www.youtube.com/watch?v=OS7neDUUhPE&ab_channel=jestarray
+
+        //Pointerover useful for when implementing hover effects, pointerout is for when it's not hovering on it
+        const startButton = this.add
+            .text(275, 400, "Start", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("SelectScene"));
+        startButton.setInteractive();
+
+        const htpButton = this.add
+            .text(400, 400, "How to Play", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () =>
+                this.add.text(400, 450, "will add later", {
+                    fontSize: "25px",
+                    color: "green",
+                })
+            );
+        htpButton.setInteractive();
     }
 
     update() {
---
+++ src/scenes/selectScene.ts
@@ -0,0 +1,110 @@
+import Phaser from "phaser";
+import FpsText from "../objects/fpsText";
+
+export default class selectScene extends Phaser.Scene {
+    fpsText: FpsText;
+
+    constructor() {
+        super({ key: "SelectScene" });
+    }
+
+    create() {
+        this.fpsText = new FpsText(this);
+
+        const message = `Phaser v${Phaser.VERSION}`;
+        this.add
+            .text(this.cameras.main.width - 15, 15, message, {
+                color: "#000000",
+                fontSize: "24px",
+            })
+            .setOrigin(1, 0);
+
+        this.add.text(300, 100, "Level Select", {
+            fontSize: "32px",
+            color: "black",
+        });
+
+        //TODO: create button class and handle all this in there to make simpler
+        //NOTE: None of the scenes for the levels have been made yet
+        const level1Button = this.add
+            .text(150, 200, "Level 1", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("Level1"));
+        level1Button.setInteractive();
+
+        const level2Button = this.add
+            .text(350, 200, "Level 2", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("Level2"));
+        level2Button.setInteractive();
+
+        const level3Button = this.add
+            .text(550, 200, "Level 3", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("Level3"));
+        level3Button.setInteractive();
+
+        const level4Button = this.add
+            .text(150, 350, "Level 4", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("Level4"));
+        level4Button.setInteractive();
+
+        const level5Button = this.add
+            .text(350, 350, "Level 5", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("Level5"));
+        level5Button.setInteractive();
+
+        const level6Button = this.add
+            .text(550, 350, "Level 6", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("Level6"));
+        level6Button.setInteractive();
+
+        const mmButton = this.add
+            .text(350, 450, "Main Menu", {
+                fontSize: "25px",
+                color: "red",
+            })
+            .on("pointerover", () => {
+                console.log("pointerover");
+            })
+            .on("pointerdown", () => this.scene.start("MainScene"));
+        mmButton.setInteractive();
+    }
+
+    update() {
+        this.fpsText.update();
+    }
+}
