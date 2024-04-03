3761972eca73716fb2f99ab5831daaaa295fbc06
Rana <rantun@udel.edu>
2024-04-03 15:02:34 -0400

    Players can now traverse board with WASD, fixed board spacing

--- src/scenes/level1PlayScene.ts
+++ src/scenes/level1PlayScene.ts
@@ -6,6 +6,15 @@ export default class Level1PlayScene extends Phaser.Scene {
         super({ key: "Level1PlayScene" });
     }
 
+    private tilesGroup: Phaser.GameObjects.Group;
+    private selectedTile: Phaser.GameObjects.Sprite;
+    private selectedTileIndex: number;
+    private keyW?: Phaser.Input.Keyboard.Key;
+    private keyA?: Phaser.Input.Keyboard.Key;
+    private keyS?: Phaser.Input.Keyboard.Key;
+    private keyD?: Phaser.Input.Keyboard.Key;
+    private prevKeyState: { [key: string]: boolean } = {};
+
     create() {
         // back to levels button
         new Button(
@@ -25,16 +34,9 @@ export default class Level1PlayScene extends Phaser.Scene {
             color: "black",
         });
 
-        //NOTE: Each block is 32x32px
-        //The padding between each block should be at least 26px (works for 9x9 board)
-        //For smaller boards the space between can be larger (28px for 7x7, 30px for 5x5)
-        // The center of the screen is 400px, so in 5x5 the middle block should have x coord 400px
-        //If you count the 26px padding and 16px from the center of each block, the next block in a row would be curr block width + 58
-        //It can be assumed that the game board also has 26px padding between the square board and the blocks
-
         const numRows = 5;
         const numCols = 5;
-        const tileSize = 64;
+        const tileSize = 32;
         const tileTypes = [
             "And",
             "Or",
@@ -42,7 +44,7 @@ export default class Level1PlayScene extends Phaser.Scene {
             "True",
             "False",
             "True",
-            "False", // add another true and false for now to give these two blocks higher chances of appearing
+            "False",
         ];
 
         function generateRandomBoard(
@@ -50,7 +52,8 @@ export default class Level1PlayScene extends Phaser.Scene {
             numCols: number,
             tileTypes: string[]
         ) {
-            //TODO?: create board class
+            //TODO?: create board class and make smarter generations
+            // Also TODO: make it so same board remains when exiting level
             const board = [];
             for (let row = 0; row < numRows; row++) {
                 const newRow = [];
@@ -66,20 +69,159 @@ export default class Level1PlayScene extends Phaser.Scene {
         }
 
         const board = generateRandomBoard(numRows, numCols, tileTypes);
+        // These coordinates are for 5x5 board to ensre it's centered
+        let startx = 280;
+        let starty = 180;
 
-        // Loop through the board and create sprites for each tile
+        // These values will be updated in loop
+        let newx = startx;
+        let newy = starty;
+
+        this.tilesGroup = this.add.group();
+
+        // Loops through board and creates sprites for each tile
         for (let row = 0; row < board.length; row++) {
             for (let col = 0; col < board[row].length; col++) {
                 const tileType = board[row][col];
-                const xPos = col * tileSize;
-                const yPos = row * tileSize;
+                // the value being added to newx/y depends on board size
+                const xPos = newx + 40;
+                const yPos = newy + 40;
 
                 const tileSprite = this.add.sprite(xPos, yPos, tileType);
-                tileSprite.setOrigin(-8, -6); // not sure if this is the best way to do this yet (just did trial and error to center the board)
+                tileSprite.setOrigin(0.5);
                 tileSprite.setData("tileType", tileType);
+
+                this.tilesGroup.add(tileSprite);
+                newx += 40;
             }
+            // Have to reset newx so row below is at same x coordinate as one above
+            newx = startx;
+            newy += 40;
         }
+
+        //Selected tile (initially the one at row 0 col 0)
+        this.selectedTileIndex = 0;
+
+        this.selectedTile =
+            this.tilesGroup.getChildren()[0] as Phaser.GameObjects.Sprite;
+
+        // Highlights selected tile
+        if (this.selectedTile) {
+            this.selectedTile.setTint(0xff0000);
+        }
+
+        // Enables WASD key input
+        this.keyW = this.input.keyboard?.addKey(
+            Phaser.Input.Keyboard.KeyCodes.W
+        );
+        this.keyA = this.input.keyboard?.addKey(
+            Phaser.Input.Keyboard.KeyCodes.A
+        );
+        this.keyS = this.input.keyboard?.addKey(
+            Phaser.Input.Keyboard.KeyCodes.S
+        );
+        this.keyD = this.input.keyboard?.addKey(
+            Phaser.Input.Keyboard.KeyCodes.D
+        );
     }
 
-    update() {}
+    update() {
+        // Had to check key state otherwise clicking once would make it move like 3 or 5 blocks
+        // Has to do with update being called so many times per second, need a workaround
+
+        //TODO: Make it so holding down a key moves you multiple times instead of pressing each time
+        if (this.keyW?.isDown && !this.prevKeyState["W"]) {
+            console.log("Pressing W");
+            this.moveSelection(0, -1);
+        } else if (this.keyS?.isDown && !this.prevKeyState["S"]) {
+            console.log("Pressing S");
+            this.moveSelection(0, 1);
+        } else if (this.keyA?.isDown && !this.prevKeyState["A"]) {
+            console.log("Pressing A");
+            this.moveSelection(-1, 0);
+        } else if (this.keyD?.isDown && !this.prevKeyState["D"]) {
+            console.log("Pressing D");
+            this.moveSelection(1, 0);
+        }
+
+        // Update previous key state so it resets
+        this.prevKeyState["W"] = this.keyW?.isDown || false;
+        this.prevKeyState["S"] = this.keyS?.isDown || false;
+        this.prevKeyState["A"] = this.keyA?.isDown || false;
+        this.prevKeyState["D"] = this.keyD?.isDown || false;
+    }
+
+    //TODO: Make moveSelection its own file that can be called for diff levels
+    moveSelection(deltaX: number, deltaY: number) {
+        const numRows = 5;
+        const numCols = 5;
+        const totalTiles = numRows * numCols;
+
+        const currentIndex = this.selectedTileIndex;
+        let newIndex = currentIndex + deltaX + deltaY * numCols;
+
+        // Wrap horizontally
+        if (deltaX !== 0) {
+            newIndex =
+                ((currentIndex + deltaX + numCols) % numCols) +
+                Math.floor(currentIndex / numCols) * numCols;
+        }
+
+        // Wrap vertically
+        if (deltaY !== 0) {
+            newIndex =
+                (currentIndex + totalTiles + deltaY * numCols) % totalTiles;
+        }
+
+        // make sure newIndex doesn't go out of range
+        // clamp function is pretty cool haven't used it till now
+        newIndex = Phaser.Math.Clamp(newIndex, 0, totalTiles - 1);
+
+        // Remove tint from previously selected tile
+        this.selectedTile.clearTint();
+
+        // Update selected tile index, cast GameObject to Sprite
+        this.selectedTileIndex = newIndex;
+        this.selectedTile = this.tilesGroup.getChildren()[
+            newIndex
+        ] as Phaser.GameObjects.Sprite;
+
+        // Highlight newly selected tile (red tint)
+        this.selectedTile.setTint(0xff0000);
+    }
+
+    /*
+    MORE COMPLEX VERSION OF GENERATE RANDOM BOARD
+    NOTE: DOES NOT WORK AT THE MOMENT
+        function generateRandomBoard(
+            numRows: number,
+            numCols: number,
+            tileTypes: string[]
+        ) {
+            const board = [];
+
+            const tileWeight: { [key: string]: number } = {
+                And: 0.1,
+                Or: 0.1,
+                Not: 0.1,
+                True: 0.35,
+                False: 0.35,
+            };
+
+            for (let row = 0; row < numRows; row++) {
+                const newRow = [];
+                for (let col = 0; col < numCols; col++) {
+                    //Use instead of random index?
+                    const randomBlock = Math.random();
+
+                    const randomIndex = Math.floor(
+                        Math.random() * tileTypes.length
+                    );
+                    newRow.push(tileTypes[randomIndex]);
+                }
+                board.push(newRow);
+            }
+            return board;
+        }
+        */
 }
