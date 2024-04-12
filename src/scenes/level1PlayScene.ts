7660e0a92ff6ee7153649c895903dae1556fdbb5
Rana <rantun@udel.edu>
2024-04-12 13:11:40 -0400

    Fixed evaluateExpression so that it correctly evaluates statements starting with "True Or..."

--- src/scenes/level1PlayScene.ts
+++ src/scenes/level1PlayScene.ts
@@ -468,6 +468,17 @@ export default class Level1PlayScene extends Phaser.Scene {
         }
     }
 
+    // Specifically for cases where "true || is the first part of an expression"
+    orEval(expression: string): boolean | null {
+        try {
+            return eval(expression) as boolean;
+        } catch (error) {
+            // Handle the error here, or simply return null if you want to ignore it
+            //console.error("Error evaluating expression:", error);
+            return null;
+        }
+    }
+
     logicalOperators: { [key: string]: string } = {
         And: "&&",
         Or: "||",
@@ -480,12 +491,53 @@ export default class Level1PlayScene extends Phaser.Scene {
         // Check if the expression starts or ends with invalid operators
         // Construct the expression string
         let result = "";
-        for (let i = 0; i < expression.length; i++) {
-            const tileType = expression[i];
-            result += this.logicalOperators[tileType];
+        if (expression[0] === "True" && expression[1] === "Or") {
+            for (let i = 2; i < expression.length; i++) {
+                const tileType = expression[i];
+                result += this.logicalOperators[tileType];
+            }
+            let calc = this.orEval(result) as boolean | null;
+            console.log(
+                "result is: " +
+                    this.logicalOperators[expression[0]] +
+                    this.logicalOperators[expression[1]] +
+                    result
+            );
+            if (calc === null) {
+                return false;
+            } else {
+                return true;
+            }
+        } else if (
+            expression[0] === "Not" &&
+            expression[1] === "False" &&
+            expression[2] === "Or"
+        ) {
+            for (let i = 3; i < expression.length; i++) {
+                const tileType = expression[i];
+                result += this.logicalOperators[tileType];
+            }
+            let calc = this.orEval(result) as boolean | null;
+            console.log(
+                "result is: " +
+                    this.logicalOperators[expression[0]] +
+                    this.logicalOperators[expression[1]] +
+                    this.logicalOperators[expression[2]] +
+                    result
+            );
+            if (calc === null) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            for (let i = 0; i < expression.length; i++) {
+                const tileType = expression[i];
+                result += this.logicalOperators[tileType];
+            }
+            console.log("result is: " + result);
+            // Evaluate the expression using eval() and return the result
+            return this.safeEval(result) as boolean;
         }
-        console.log("result is: " + result);
-        // Evaluate the expression using eval() and return the result
-        return this.safeEval(result) as boolean;
     }
 }
